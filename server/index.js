12a13cedf3287951a04df5c8613c116ed8ac692e
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-06-14 18:02:07 +0200

    Modiefied routes that the server will use

--- server/index.js
+++ server/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const cors = require('cors');
+app.use(cors());
+
 mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test');
-//mongoose.set('useFindAndModify', false);
-const routes = require('./src/routes/movieRoutes');
 
-routes(app);
+const routes = require('./src/routes/authRoutes');
+app.use(routes);
 
-app.listen(3000,  ()=>{
+app.listen(3000, ()=>{
     console.log('Listening on port 3000')
 })
