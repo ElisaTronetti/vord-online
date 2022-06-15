a75ec306b857ec6e2def4a42c4d8fdb592e323bc
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-06-15 14:29:44 +0200

    Included chonky dependency and added component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { FullFileBrowser } from 'chonky';
 import './App.css';
 
 function App() {
+  const files = [
+    { id: 'lht', name: 'Projects', isDir: true },
+    {
+      id: 'mcd',
+      name: 'chonky-sphere-v2.png',
+      thumbnailUrl: 'https://chonky.io/chonky-sphere-v2.png',
+    },
+  ];
+  const folderChain = [{ id: 'xcv', name: 'Demo', isDir: true }];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: 300 }}>
+      <FullFileBrowser files={files} folderChain={folderChain} />
     </div>
   );
 }
