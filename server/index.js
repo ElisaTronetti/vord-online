41d6c6beddbdbe2fab310a3342cad5025035facb
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-06-15 14:28:11 +0200

    Changed ip for mongo db

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ const cors = require('cors');
 app.use(cors());
 
 mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27018/test');
+mongoose.connect('mongodb://localhost:27017/test');
 
 const authRoutes = require('./src/routes/authRoutes');
 const fileSystemRoutes =  require('./src/routes/fileSystemRoutes');
 
 app.use(authRoutes);
-app.use(fileSystemRoutes);
+//app.use(fileSystemRoutes);
 
 app.listen(3000, ()=>{
     console.log('Listening on port 3000')
