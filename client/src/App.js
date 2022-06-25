baedfb77ae876f30e04d4da358a9bd3051ed07ba
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-06-25 11:00:14 +0200

    Created login component and routing

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import {
 } from 'react-router-dom'
 
 import Home from './home/Home'
+import Login from './login/Login'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/home' element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/login/Login.js
@@ -0,0 +1,6 @@
+
+export default function Login() {
+    return (
+        <p>login</p>
+    )
+}
