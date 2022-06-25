c023184770279e18613fb12d4a3f49585514e5ef
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-06-25 12:26:58 +0200

    Added redux store, actions and reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import { setChonkyDefaults } from 'chonky'
 import { ChonkyIconFA } from 'chonky-icon-fontawesome'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import allReducers from './redux'
 
 import Home from './home/Home'
 import Login from './login/Login'
 
 setChonkyDefaults({ iconComponent: ChonkyIconFA })
 
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/home' element={<Home/>}/>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/home' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
--- client/src/login/Login.js
+++ client/src/login/Login.js
@@ -1,14 +1,25 @@
 import { useState } from 'react'
 import { Navigate, Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { increment, setValue } from '../redux/counter/actions'
 
 export default function Login() {
     const [reroute, setReroute] = useState(false)
+
+    const dispatch = useDispatch() //for the actions use for redux
+    let counterValue = useSelector(state => state.counter.value)
+
     if (reroute) {
         return <Link to={'/home'}><Navigate to={'/home'}/></Link>
     } else {
         return (
-            <Button variant='danger' onClick={() => setReroute(true)}>login</Button>
+            <div>
+                <p onClick={() => dispatch(increment())}>{counterValue}</p>
+                <Button onClick={() => dispatch(setValue(42))}>Set 42</Button>
+                <Button variant='danger' onClick={() => setReroute(true)}>login</Button>
+            </div>
         )
     } 
 }
