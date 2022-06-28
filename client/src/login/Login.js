3be6d6356fa9669b88256305357221620dc8b2a1
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-06-28 18:43:05 +0200

    After login the user is  redirected to home

--- client/src/login/Login.js
+++ client/src/login/Login.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { Navigate, Link } from 'react-router-dom'
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import Button from 'react-bootstrap/Button'
@@ -8,7 +9,6 @@ import userLogin from './userLogin'
 
 export default function Login() {
     const dispatch = useDispatch()
-
     const [inputEmail, setInputEmail] = useState(undefined)
     const [inputPassword, setInputPassword] = useState(undefined)
 
@@ -16,41 +16,38 @@ export default function Login() {
         userLogin(inputEmail, inputPassword, dispatch)
     }
 
-    return (
-        <Row className="d-flex justify-content-center">
-            <div className="my-5 container col-lg-3 col-9 border border-success rounded trnsp">
-                <Form className="mt-1 mb-3">
-                    <h1 className="d-flex justify-content-center">Login</h1>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control
-                            type="email"
-                            onChange={input => setInputEmail(input.target.value)}
-                            onEnter={tryLogin}
-                            placeholder="Enter email" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            type="password"
-                            onChange={input => setInputPassword(input.target.value)}
-                            onEnter={tryLogin}
-                            placeholder="Password" />
-                    </Form.Group>
-                    <div class="text-center">
-                        <Button variant="primary" onClick={tryLogin}>Login</Button>
-                        <p>Not a member? <a href="/signup">Register</a></p>
-                    </div>
-                </Form>
-            </div>
-        </Row>
-    )
+    let token = useSelector(state => state.userData.token)
+    if (token !== null){
+        return <Link to={'/home'}><Navigate to={'/home'}/></Link>
+    } else {
+        return (
+            <Row className="d-flex justify-content-center">
+                <div className="my-5 container col-lg-3 col-9 border border-success rounded trnsp">
+                    <Form className="mt-1 mb-3">
+                        <h1 className="d-flex justify-content-center">Login</h1>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                onChange={input => setInputEmail(input.target.value)}
+                                onEnter={tryLogin}
+                                placeholder="Enter email" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                onChange={input => setInputPassword(input.target.value)}
+                                onEnter={tryLogin}
+                                placeholder="Password" />
+                        </Form.Group>
+                        <div class="text-center">
+                            <Button variant="primary" onClick={tryLogin}>Login</Button>
+                            <p>Not a member? <a href="/signup">Register</a></p>
+                        </div>
+                    </Form>
+                </div>
+            </Row>
+        )
+    }
 }
-
-/*
-REDUX INTERACTIONS
-<div>
-    <p onClick={() => dispatch(increment())}>{counterValue}</p>
-    <Button onClick={() => dispatch(setValue(42))}>Set 42</Button>
-    <Button variant='danger' onClick={() => setReroute(true)}>login</Button>
-</div> */
