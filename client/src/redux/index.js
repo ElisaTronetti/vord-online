4c28f6930ea0ed3397c256a8126eb39a866bc0d4
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-07-10 16:35:53 +0200

    Fixed deprecated createStorage with correct function configureStore (removed the reducer combiners)

--- client/src/redux/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import userDataReducer from './userData/reducer'
-
-const allReducers = combineReducers({
-    userData: userDataReducer,
-})
-
-export default allReducers
