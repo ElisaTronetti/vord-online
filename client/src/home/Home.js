02ebe2195e1ce08abf508e824ed1b61382849fec
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-07-12 19:02:20 +0200

    Added useEffect to control if the state is changed, in order to call the request to update the file system

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,18 +1,27 @@
 import { FullFileBrowser, ChonkyActions } from 'chonky'
-import { useState, useMemo } from 'react'
+import { useState, useMemo, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { updateFileSystem } from '../home/fileSystem'
 
-import { useFiles, useFileActionHandler, useFolderChain, deleteFiles, moveFiles } from './fileSystemNavigator'
+import { useFiles, useFileActionHandler, useFolderChain } from '../fileSystemUtils/fileSystemNavigator'
 
 export default function Home() {
-  const dispatch = useDispatch()
+  let id = useSelector(state => state.userData.id)
+  let token = useSelector(state => state.userData.token)
   let rootFolderId = useSelector(state => state.fileSystemData.rootFolderId)
   let fileMap = useSelector(state => state.fileSystemData.fileMap)
 
   const [currentFolderId, setCurrentFolderId] = useState(rootFolderId)
 
+  useEffect(() => {
+    let fileSystem = recreateFileSystem(rootFolderId, fileMap)
+    //calling HTTP request in order to update the fileSystem when something changes
+    updateFileSystem(id, token, fileSystem)
+  }, [rootFolderId, fileMap, id, token])
+
+  const dispatch = useDispatch()
   const files = useFiles(fileMap, currentFolderId)
-  const handleFileAction = useFileActionHandler(fileMap, setCurrentFolderId, deleteFiles, moveFiles, dispatch)
+  const handleFileAction = useFileActionHandler(fileMap, setCurrentFolderId, dispatch)
   const folderChain = useFolderChain(fileMap, currentFolderId)
 
   const fileActions = useMemo(
@@ -25,4 +34,9 @@ export default function Home() {
       <FullFileBrowser files={files} fileActions={fileActions} onFileAction={handleFileAction} folderChain={folderChain}/>
     </div>
   )
+}
+
+function recreateFileSystem(rootFolderId, fileMap){
+  return JSON.parse('{"rootFolderId":"' + rootFolderId + '", ' +
+  '"fileMap":' + JSON.stringify(fileMap) + '}')
 }
