84f2249c4141174f3a4b24b10df2f2e42c151f4d
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-07-13 15:27:03 +0200

    Added toasts in login to inform the user about the result of the login

--- client/src/login/userLogin.js
+++ client/src/login/userLogin.js
@@ -1,39 +1,41 @@
 import $ from 'jquery'
 import { setToken, setId } from "../redux/userData/actions"
-import { setRootFolderId, setFileMap} from '../redux/fileSystemData/actions'
+import { setRootFolderId, setFileMap } from '../redux/fileSystemData/actions'
 import { createSuccessToast, createErrorToast, createWarningToast } from '../toast/createToast'
 
 export default function userLogin(email, password, dispatch) {
     if (email !== '' && password !== '') {
-        $.ajaxSetup({
-            contentType: "application/json; charset=utf-8"
-        })
-        $.post(process.env.REACT_APP_SERVER + "auth/login", createParams(email, password))
-            .done(function (result) {
-
+        $.ajax({
+            contentType: 'application/json',
+            dataType: 'json',
+            data: createParams(email, password),
+            success: function (result) {
+                // Save token
                 let token = result.token
                 dispatch(setToken(token))
-
+                // Save user id
                 let id = result._id
                 dispatch(setId(id))
-                
+                // Save file system if it exists
                 if (result.fileSystem !== undefined) {
                     let fileSystem = result.fileSystem
-                    console.log('A file system exists ' + JSON.stringify(fileSystem))
                     dispatch(setRootFolderId(fileSystem.rootFolderId))
                     dispatch(setFileMap(fileSystem.fileMap))
                 }
                 createSuccessToast('Login successful!')
-
-            })
-            .fail(function (result) {
-                console.log(result)
-            })
+            },
+            error: function () {
+                createErrorToast('Error: enable to login')
+            },
+            type: 'POST',
+            url: process.env.REACT_APP_SERVER + "auth/login"
+        })
     } else {
-        console.log('Missing data')
+        createWarningToast('Missing required data')
     }
 }
 
+// Create body params
 function createParams(email, password) {
     return JSON.stringify({
         email: email,
