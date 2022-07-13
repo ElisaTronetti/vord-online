fa716a218cf6fc6a53eb10d0afb53635ff26d65c
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-07-13 16:34:47 +0200

    Added comments in Home

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,30 +1,34 @@
 import { FullFileBrowser, ChonkyActions } from 'chonky'
 import { useState, useMemo, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { updateFileSystem } from '../home/fileSystem'
-import { CreateDocument } from '../fileSystemUtils/actions'
 
+import { CreateDocument } from '../fileSystemUtils/actions'
+import { updateFileSystem } from './fileSystemRequests'
 import { useFiles, useFileActionHandler, useFolderChain } from '../fileSystemUtils/fileSystemNavigator'
 
 export default function Home() {
+  const dispatch = useDispatch()
+
+  const [currentFolderId, setCurrentFolderId] = useState(rootFolderId)
+
   let id = useSelector(state => state.userData.id)
   let token = useSelector(state => state.userData.token)
   let rootFolderId = useSelector(state => state.fileSystemData.rootFolderId)
   let fileMap = useSelector(state => state.fileSystemData.fileMap)
 
-  const [currentFolderId, setCurrentFolderId] = useState(rootFolderId)
-
+  // Trigger used to update the file system on the server when something changes
   useEffect(() => {
     let fileSystem = recreateFileSystem(rootFolderId, fileMap)
-    //calling HTTP request in order to update the fileSystem when something changes
+    // Calling HTTP request
     updateFileSystem(id, token, fileSystem)
   }, [rootFolderId, fileMap, id, token])
 
-  const dispatch = useDispatch()
+  // Initialize data for the file system library
   const files = useFiles(fileMap, currentFolderId)
   const handleFileAction = useFileActionHandler(fileMap, setCurrentFolderId, currentFolderId, dispatch)
   const folderChain = useFolderChain(fileMap, currentFolderId)
 
+  // Initialize actions
   const fileActions = useMemo(
     () => [ChonkyActions.DeleteFiles, ChonkyActions.CreateFolder, CreateDocument],
     []
@@ -37,6 +41,7 @@ export default function Home() {
   )
 }
 
+// Used to recreate the file system JSON data structure
 function recreateFileSystem(rootFolderId, fileMap){
   return JSON.parse('{"rootFolderId":"' + rootFolderId + '", ' +
   '"fileMap":' + JSON.stringify(fileMap) + '}')
