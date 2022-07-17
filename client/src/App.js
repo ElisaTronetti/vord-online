eb9d6b2903d9fb7dc7ba7b74b7ef788c88da2ccb
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-07-17 11:45:21 +0200

    Added home in navbar to go back to the home

--- client/src/App.js
+++ client/src/App.js
@@ -25,14 +25,14 @@ function App() {
   return (
     <div>
       <Provider store={store}>
-        <CustomNavbar />
         <ToastContainer />
         <BrowserRouter>
+        <CustomNavbar />
           <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/editor' element={<Editor />} />
+              <Route path='/' element={<Login />} />
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/home' element={<Home />} />
+              <Route path='/editor' element={<Editor />} />
           </Routes>
         </BrowserRouter>
       </Provider>
--- client/src/commonComponents/CustomNavbar.js
+++ client/src/commonComponents/CustomNavbar.js
@@ -1,22 +1,32 @@
-import { Col } from "react-bootstrap"
-import { Navbar, Nav } from "react-bootstrap"
+import Navbar from "react-bootstrap/Navbar"
 import Container from 'react-bootstrap/Container'
-
-//import logo from '../../public/logo.svg'
+import Nav from 'react-bootstrap/Nav'
+import { useSelector } from 'react-redux'
+import { Link } from "react-router-dom"
 
 export default function CustomNavbar() {
+  let token = useSelector(state => state.userData.token)
+
   return (
-
-      <Navbar bg="dark" variant="dark">
-        <Container fluid >
-          <div>
-          <Navbar.Brand>
-            <img width="30%" height="30%" className="align-center" alt="logo" src='/full_logo.png' />{' '}  
-          </Navbar.Brand>
-          </div>
-          
-        </Container>
-      </Navbar>
-
+    <Navbar bg="dark" variant="dark">
+      <Container fluid >
+        <Navbar.Brand>
+          <img width="70" height="70" className="align-center" alt="logo" src='/logo512.png' />{' '}
+        </Navbar.Brand>
+        {
+          token !== null && (
+            <>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse className="justify-content-end">
+                <Nav >
+                    <Nav.Link >
+                      <Link to='/home' style={{ color: 'inherit', textDecoration: 'inherit'}}>Home</Link>
+                    </Nav.Link>         
+                </Nav>
+              </Navbar.Collapse>
+            </>
+          )}
+      </Container>
+    </Navbar>
   )
 }
