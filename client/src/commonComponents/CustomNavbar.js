6593af4386b6f22b6f3384fe2dcf4b45b90673ff
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-07-17 19:25:41 +0200

    Added icons in custom navbar

--- client/src/commonComponents/CustomNavbar.js
+++ client/src/commonComponents/CustomNavbar.js
@@ -4,6 +4,8 @@ import Nav from 'react-bootstrap/Nav'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { RiHome2Fill } from 'react-icons/ri'
+import { MdNotifications } from 'react-icons/md'
+import { ImExit } from 'react-icons/im'
 
 export default function CustomNavbar() {
   let token = useSelector(state => state.userData.token)
@@ -21,6 +23,8 @@ export default function CustomNavbar() {
               <Navbar.Collapse className="justify-content-end">
                 <Nav>
                   <Link to='/home'><RiHome2Fill size={40} color="white" /></Link>
+                  <MdNotifications size={40} color="white" />
+                  <ImExit size={40} color="white" />
                 </Nav>
               </Navbar.Collapse>
             </>
