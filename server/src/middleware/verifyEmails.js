e0471d9bea31926ffcf143c35a594993be60c713
AndreaIngargiola <92037242+AndreaIngargiola@users.noreply.github.com>
2022-07-29 12:03:09 +0200

    add verifyEmail middleware (tested and functioning)

--- server/src/middleware/verifyEmails.js
+++ server/src/middleware/verifyEmails.js
@@ -1,17 +1,31 @@
+const Responses = require("../controllers/responses/response")
+const Users = require("../models/userModel")
+
 const verifyEmails = async (req, res, next) => {
-  if (!sharedWith) {
-    return res.status(403).send("sharedWith array not found");
+  const sharedWith = req.body.sharedWith
+  console.log(sharedWith)
+  if (sharedWith === undefined) {
+    return res.status(403).send("sharedWith array not found");  //sharedWith doesn't exist (bad request)
   }
   try {
     let i = 0, result
-    while(!sharedWith[i]){
-      if(!sharedWith[i].email) { return res.status(403).send("Missing parameter: sharedWith["+i+"].email") }
-      result = await Users.findOne({email: sharedWith[i].email})
-      if(!result) {return res.status(404).send("User not found: "+sharedWith[i].email)}
+    while(sharedWith[i] !== undefined){
+      //sharedWith[i] doesn't have an email field (bad request)
+      if(sharedWith[i].email === undefined) { return res.status(403).send("Missing parameter: sharedWith["+i+"].email") }  
+
+      //check if the email corresponds to a registered user
+      result = await Users.exists({email: sharedWith[i].email})
+      
+      if(!result) { return res.status(404).send("User not found: " + sharedWith[i].email) }   //if the email doesn't exist in the database, return error
+      
+      i++
     }
+
+    //if every mail exists in the databse the computation can continue
     return next()
+    
   } catch (err) {
-    return res.status(403).send("Invalid user");
+    return Responses.ServerError(res, {message: err.message});
   }
 };
 
--- server/src/routes/shaDocRoutes.js
+++ server/src/routes/shaDocRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/shaDocController');
 const clearance = require('../middleware/clearance')
+const verifyEmails = require('../middleware/verifyEmails')
 
 /*
     body params: 
@@ -18,9 +19,9 @@ const clearance = require('../middleware/clearance')
         documentId
 
 */
-router.post("/sharedDocuments/shareLocalDocument",  (req, res) => controller.shareLocalDocument(req, res));
+router.post("/sharedDocuments/shareLocalDocument", verifyEmails, (req, res) => controller.shareLocalDocument(req, res));
 
 //body param: same as before
-router.post("/sharedDocuments/shareSharedDocument", clearance(2), (req, res) => controller.shareSharedDocument(req, res));
+router.post("/sharedDocuments/shareSharedDocument", clearance(2), verifyEmails, (req, res) => controller.shareSharedDocument(req, res));
 
 module.exports = router;
