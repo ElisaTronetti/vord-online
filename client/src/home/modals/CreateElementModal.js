7c6153b3e5d7213e69565343214a5afe73dbed4d
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-08-09 11:54:09 +0200

    The modal for the creation of folder and documents are automatically focussed

--- client/src/home/modals/CreateElementModal.js
+++ client/src/home/modals/CreateElementModal.js
@@ -3,13 +3,16 @@ import { useSelector, useDispatch } from 'react-redux'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
+import ObjectID from 'bson-objectid'
 
+import { createDocument } from '../fileSystemUtils/modifyFileSystem'
+import { createNewDocument } from '../documentsUtils/documentRequests'
 import { createFolder } from '../fileSystemUtils/modifyFileSystem'
 
-export default function CreateFolderModal(props) {
-    const [inputFolderName, setInputFolderName] = useState("")
-    const dispatch = useDispatch()
+export default function CreateElementModal(props) {
+    const [inputName, setInputName] = useState("")
     const inputRef = useRef()
+    const dispatch = useDispatch()
     const user = {
         id: useSelector(state => state.userData.id),
         token: useSelector(state => state.userData.token)
@@ -19,12 +22,32 @@ export default function CreateFolderModal(props) {
         fileMap: useSelector(state => state.fileSystemData.fileMap)
     }
 
-    function tryCreateFolder() {
-        if (inputFolderName !== "") {
-            createFolder(user, fileSystem, props.currentFolderId, inputFolderName, dispatch)
+    function tryCreateDocument() {
+        if (inputName !== "") {
+            let documentId = ObjectID().toHexString()
+            // Create document in the file system
+            createDocument(user, fileSystem, props.currentFolderId, documentId, inputName, dispatch)
+            // Trigger HTTP request to create document in the list of documents in the server
+            createNewDocument(user, documentId, inputName)
             props.onHide()
         }
     }
+
+    function tryCreateFolder() {
+        if (inputName !== "") {
+            createFolder(user, fileSystem, props.currentFolderId, inputName, dispatch)
+            props.onHide()
+        }
+    }
+
+    /*function elementTypeCreation() {
+        if (props.type === 'folder') {
+            tryCreateFolder()
+        } else if (props.type === 'document') {
+            tryCreateDocument()
+        }
+    }*/
+    
 
     return (
         <Modal
@@ -35,22 +58,21 @@ export default function CreateFolderModal(props) {
             centered
             onShow={() => { inputRef.current.focus() }}>
             <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">Create folder</Modal.Title>
+                <Modal.Title id="contained-modal-title-vcenter">{'Create ' + props.type}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <Form>
                     <Form.Group className="mb-3" controlId="formFolderName">
-                        <Form.Label>Folder name</Form.Label>
                         <Form.Control
                             ref={inputRef}
-                            onChange={input => setInputFolderName(input.target.value)}
+                            onChange={input => setInputName(input.target.value)}
                             onKeyPress={event => { if (event.key === "Enter") { event.preventDefault(); tryCreateFolder() } }}
-                            placeholder="Enter new folder name" />
+                            placeholder={"Enter new " + props.type + " name"} />
                     </Form.Group>
                 </Form>
             </Modal.Body>
             <Modal.Footer>
-                <Button onClick={tryCreateFolder}>Save</Button>
+                <Button onClick={(props.isFolder ? tryCreateFolder : tryCreateDocument())}>Save</Button>
             </Modal.Footer>
         </Modal>
     )
