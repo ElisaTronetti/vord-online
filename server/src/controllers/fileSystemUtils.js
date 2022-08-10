061d5dc9d4da81a169474b8d17d440292808e44a
AndreaIngargiola <92037242+AndreaIngargiola@users.noreply.github.com>
2022-08-10 19:27:58 +0200

    add server side file system creation on user signup

---
+++ server/src/controllers/fileSystemUtils.js
@@ -0,0 +1,3 @@
+const ObjectId = require('mongoose').Types.ObjectId
+const Responses = require("./responses/response")
+const Users = require('../models/userModel')
--- server/src/models/factories/user.js
+++ server/src/models/factories/user.js
@@ -1,6 +1,23 @@
 const User = require('../userModel')
+const ObjectId = require('mongoose').Types.ObjectId
 
-function createUser(email, name, surname, password, fileSystem) {
+function createUser(email, name, surname, password) {
+    let newId = new ObjectId()
+    const rootFolderId = newId.toString()
+    const fileSystem = {
+      rootFolderId : rootFolderId,
+      fileMap: {
+        [newId] : {
+          id: newId.toString(),
+          name: "Home",
+          isShared: false,
+          isDir: true,
+          childrenCounts: 0,
+          childrenIds: []
+        }
+      }
+    }
+    console.log(fileSystem)
     return new User({
         email: email,
         name: name,
