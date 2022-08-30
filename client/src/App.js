429e1afc30244adb407bdbb0de6e7de11ced778c
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-08-30 16:24:55 +0200

    Fixed persist storage of redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
 import { setChonkyDefaults } from 'chonky'
 import { ChonkyIconFA } from 'chonky-icon-fontawesome'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { SocketContext, socket } from './util/socketContext'
+import { persistor, store } from './util/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
-import allReducers from './redux'
 import Home from './home/Home'
 import Login from './authentication/Login'
 import Signup from './authentication/Signup'
@@ -17,25 +17,22 @@ import CustomNavbar from './commonComponents/CustomNavbar'
 
 setChonkyDefaults({ iconComponent: ChonkyIconFA })
 
-// Configure reedux store and its reducers
-const store = configureStore({
-  reducer: allReducers
-})
-
 function App() {
   return (
     <Provider store={store}>
       <SocketContext.Provider value={socket}>
-        <ToastContainer />
-        <BrowserRouter>
-          <CustomNavbar />
-          <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/editor' element={<Editor />} />
-          </Routes>
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <ToastContainer />
+          <BrowserRouter>
+            <CustomNavbar />
+            <Routes>
+              <Route path='/' element={<Login />} />
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/home' element={<Home />} />
+              <Route path='/editor' element={<Editor />} />
+            </Routes>
+          </BrowserRouter>
+        </PersistGate>
       </SocketContext.Provider>
     </Provider>
   )
---
+++ client/src/util/store.js
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit'
+import allReducers from '../redux'
+import storage from 'redux-persist/lib/storage'
+import thunk from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+
+const persistConfig = {
+  key: 'user',
+  storage
+}
+
+// Using the created combined reducer to create a persist reducer
+const persistedReducer = persistReducer(persistConfig, allReducers)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk]
+})
+
+export const persistor = persistStore(store)
