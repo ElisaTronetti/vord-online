38afb9f7cccd19c8b2b4d1477ab810ccf0f5cf33
AndreaIngargiola <92037242+AndreaIngargiola@users.noreply.github.com>
2022-08-31 16:56:57 +0200

    add auth middleware in shared odcuments routes

--- server/src/routes/shaDocRoutes.js
+++ server/src/routes/shaDocRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/shaDocController');
 const clearance = require('../middleware/clearance')
+const auth = require('../middleware/auth')
 const verifyEmails = require('../middleware/verifyEmails')
 
 /*
@@ -21,27 +22,27 @@ const verifyEmails = require('../middleware/verifyEmails')
 */
 
 //user, sharedWith, documentId (body)
-router.post("/sharedDocuments/shareLocalDocument", verifyEmails, (req, res) => controller.shareLocalDocument(req, res));
+router.post("/sharedDocuments/shareLocalDocument", auth, verifyEmails, (req, res) => controller.shareLocalDocument(req, res));
 
 //user, sharedWith, documentId (body)
-router.post("/sharedDocuments/shareSharedDocument", clearance(3), verifyEmails, (req, res) => controller.shareSharedDocument(req, res));
+router.post("/sharedDocuments/shareSharedDocument", auth, clearance(3), verifyEmails, (req, res) => controller.shareSharedDocument(req, res));
 
 //user, sharedWith, documentId (body)
-router.post("/sharedDocuments/manageSharedGroup", clearance(3), verifyEmails, (req, res) => controller.manageSharedGroup(req, res));
+router.post("/sharedDocuments/manageSharedGroup", auth, clearance(3), verifyEmails, (req, res) => controller.manageSharedGroup(req, res));
 
 //userid, documentid (simple header param)
-router.get("/sharedDocuments/getSharedGroup", clearance(1), (req, res) => controller.getSharedGroup(req, res));
+router.get("/sharedDocuments/getSharedGroup", auth, clearance(1), (req, res) => controller.getSharedGroup(req, res));
 
 //userid, documentid (simple header param)
-router.get("/sharedDocuments/getSharedDocument", clearance(1), (req, res) => controller.getSharedDocument(req, res));
+router.get("/sharedDocuments/getSharedDocument", auth, clearance(1), (req, res) => controller.getSharedDocument(req, res));
 
 //user, documentId, blocks (body)
-router.post("/sharedDocuments/saveSharedDocument", clearance(2), (req, res) => controller.saveSharedDocument(req, res));
+router.post("/sharedDocuments/saveSharedDocument", auth, clearance(2), (req, res) => controller.saveSharedDocument(req, res));
 
 //user, documentId (body)
-router.post("/sharedDocuments/deleteForMe", clearance(1), (req, res) => controller.deleteForMe(req, res));
+router.post("/sharedDocuments/deleteForMe", auth, clearance(1), (req, res) => controller.deleteForMe(req, res));
 
 //user, documentId (body)
-router.post("/sharedDocuments/deleteForAll", clearance(3), (req, res) => controller.deleteForAll(req, res));
+router.post("/sharedDocuments/deleteForAll", auth, clearance(3), (req, res) => controller.deleteForAll(req, res));
 
 module.exports = router;
